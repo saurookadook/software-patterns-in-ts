24d2357ffcdbbceff1cc49a89186f7a371521af7
Andy Maskiell <maskiella@gmail.com>
2025-04-03 21:38:54 -0400

    Implement rest of BuilderApp :]

--- packages/design-patterns/creational/builders/BuilderApp.ts
+++ packages/design-patterns/creational/builders/BuilderApp.ts
@@ -1,11 +1,10 @@
-import { EngineTypes } from "./constants";
-import { Car, Director, Manual } from "./entities";
+// import { EngineTypes } from './constants';
+import { Car, Director, Manual } from './entities';
+import { CarBuilder } from './CarBuilder';
+import { CarManualBuilder } from './CarManualBuilder';
+import { Builder, CarConfig } from './types';
 
-type CarConfig = {
-    seats: number;
-}
-
-type ResultProduct = [Car, Manual]
+export type ResultProduct = [Car, Manual]
 
 /**
  * @description The client code creates a builder object, passes it to the \
@@ -14,19 +13,32 @@ type ResultProduct = [Car, Manual]
  */
 export class BuilderApp {
     private director: Director;
+    private resultProducts: ResultProduct[] = [];
+    private builder: Builder<Car> | Builder<Manual>;
 
     constructor() {
         this.director = new Director();
     }
 
-    makeCars(carConfigs: CarConfig[]): ResultProduct[] {
-        const resultProducts: ResultProduct[] = [];
-
+    makeCars(carConfigs: CarConfig[]): void {
         if (!Array.isArray(carConfigs) || carConfigs.length <= 0) {
             console.warn('Skipping car creation. No car configs provided.');
-            return resultProducts;
         }
 
-        return resultProducts;
+        for (const carConfig of carConfigs) {
+            this.builder = new CarBuilder();
+            this.director.constructCarFromConfig(this.builder, carConfig);
+            const car = this.builder.getProduct();
+
+            this.builder = new CarManualBuilder();
+            this.director.constructCarFromConfig(this.builder, carConfig);
+            const manual = this.builder.getProduct();
+
+            this.resultProducts.push([car, manual]);
+        }
+    }
+
+    getResultProducts(): ResultProduct[] {
+        return this.resultProducts;
     }
 }
